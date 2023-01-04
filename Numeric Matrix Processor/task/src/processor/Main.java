e45f3430763705684717b7f66613074458d38115
Sergei857 <32968405+Sergei857@users.noreply.github.com>
2023-01-04 15:30:26 +0300

    2.1

--- Numeric Matrix Processor/task/src/processor/Main.java
+++ Numeric Matrix Processor/task/src/processor/Main.java
@@ -6,6 +6,19 @@ import java.io.InputStreamReader;
 
 public class Main {
     public static void main(String[] args) throws IOException {
+        while (true) {
+            System.out.println("1. Add matrices");
+            System.out.println("2. Multiply matrix by a constant");
+            System.out.println("3. Multiply matrices");
+            System.out.println("0. Exit");
+            System.out.print("Your choice: ");
+            int chois = Integer.parseInt(InputStr());
+            if (chois == 0) {
+                break;
+            }
+        }
+
+
         String[] size_m1 = InputStr().split("\\s+");
         Matrica m1 = new Matrica(Integer.parseInt(size_m1[0]), Integer.parseInt(size_m1[1]));
         for (int row = 0; row < m1.rows_size; row++) {
@@ -14,21 +27,25 @@ public class Main {
                 m1.arr[row][col] = Integer.parseInt(line_of_m[col]);
             }
         }
-        String[] size_m2 = InputStr().split("\\s+");
-        Matrica m2 = new Matrica(Integer.parseInt(size_m2[0]), Integer.parseInt(size_m2[1]));
-        for (int row = 0; row < m2.rows_size; row++) {
+
+
+        int cons = Integer.parseInt(InputStr());
+        Matrica m = Matrica.mulm(m1, cons);
+        Matrica.printM(m);
+    }
+
+    public static Matrica inputmatrix() throws IOException {
+        System.out.print("Enter size of first matrix: ");
+        String[] size_m = InputStr().split("\\s+");
+        Matrica m = new Matrica(Integer.parseInt(size_m[0]), Integer.parseInt(size_m[1]));
+        System.out.println("Enter second matrix:");
+        for (int row = 0; row < m.rows_size; row++) {
             String[] line_of_m = InputStr().split("\\s+");
-            for (int col = 0; col < m2.columns_size; col++) {
-                m2.arr[row][col] = Integer.parseInt(line_of_m[col]);
+            for (int col = 0; col < m.columns_size; col++) {
+                m.arr[row][col] = Integer.parseInt(line_of_m[col]);
             }
         }
-        Matrica m = Matrica.addmm(m1, m2);
-        if (m == null) {
-            System.out.print("ERROR");
-
-        } else {
-            Matrica.printM(m);
-        }
+        return m;
     }
 
 
--- Numeric Matrix Processor/task/src/processor/Matrica.java
+++ Numeric Matrix Processor/task/src/processor/Matrica.java
@@ -12,10 +12,7 @@ public class Matrica {
         this.arr = new Integer[rows_size][columns_size];
     }
 
-    public static Matrica addmm(Matrica madd1, Matrica madd2) {
-        if ((madd1.rows_size != madd2.rows_size) || (madd1.columns_size != madd2.columns_size)) {
-            return null;
-        }
+    public static Matrica add(Matrica madd1, Matrica madd2) {
         Matrica m = new Matrica(madd1.rows_size, madd1.columns_size);
         for (int r = 0; r < madd1.rows_size; r++) {
             for (int c = 0; c < madd1.columns_size; c++) {
@@ -24,6 +21,28 @@ public class Matrica {
         }
         return m;
     }
+
+    public static Matrica mulconstant(Matrica madd1, int cons) {
+        Matrica m = new Matrica(madd1.rows_size, madd1.columns_size);
+        for (int r = 0; r < madd1.rows_size; r++) {
+            for (int c = 0; c < madd1.columns_size; c++) {
+                m.arr[r][c] = madd1.arr[r][c] * cons;
+            }
+        }
+        return m;
+    }
+
+
+    public static Matrica mulmatrix(Matrica madd1, Matrica madd2) {
+        Matrica m = new Matrica(madd1.rows_size, madd1.columns_size);
+        for (int r = 0; r < madd1.rows_size; r++) {
+            for (int c = 0; c < madd1.columns_size; c++) {
+                m.arr[r][c] = madd1.arr[r][c];
+            }
+        }
+        return m;
+    }
+
 
     public static void printM(Matrica m) {
         for (int row = 0; row < m.rows_size; row++) {
